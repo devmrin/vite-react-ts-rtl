8452ae80d98774e73562b907fd3356a36e9e86b0
Mrinmay Mukherjee <mrinmay@Mrinmays-MacBook-Pro.local>
2023-12-17 13:43:01 +0530

    feat: add reset

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -16,4 +16,16 @@ describe("Simple working test", () => {
 		await userEvent.click(counter);
 		expect(counter.textContent).toBe("count is 1");
 	});
+
+	it("should reset count on click", async () => {
+		render(<App />);
+		const counter = screen.getByRole("button", { name: /count is/i });
+		expect(counter.textContent).toBe("count is 0");
+		await userEvent.click(counter);
+		expect(counter.textContent).toBe("count is 1");
+		const reset = screen.getByRole("button", { name: /reset count/i });
+		await userEvent.click(reset);
+		expect(counter.textContent).toBe("count is 0");
+	});
+	
 });
